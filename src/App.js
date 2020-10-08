7071267281c1d2122420e5dc08ece58f11090050
straylight <bs+github@straylight.capital>
2020-10-07 23:43:52 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg">
+      <header className="header">
+      {/* <p>
+        straylight::capital &#123;&#125; ;
+      </p> */}
+      <p>
+        straylight.capital_
+      </p>
+        <a
+          className="contact"
+          href="mailto:info@straylight.capital"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          contact
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
