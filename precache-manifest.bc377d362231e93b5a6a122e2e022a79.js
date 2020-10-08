9d471684a2570103f0f0acbd2abd12573db92578
Bryan Sun <bs+github@straylight.capital>
2020-10-08 00:38:15 -0500

    Updates

---
+++ precache-manifest.bc377d362231e93b5a6a122e2e022a79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c6bd5f088c3576f94cb2ca07bfbb14c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5db3d3dcd0e3f02376fe",
+    "url": "/static/css/main.9180c631.chunk.css"
+  },
+  {
+    "revision": "2bfef7bb0bcd801d73dd",
+    "url": "/static/js/2.303d917d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.303d917d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5db3d3dcd0e3f02376fe",
+    "url": "/static/js/main.2efb0ba2.chunk.js"
+  },
+  {
+    "revision": "dfe3d817a9ac0c05d2c9",
+    "url": "/static/js/runtime-main.84cfcdfc.js"
+  }
+]);
